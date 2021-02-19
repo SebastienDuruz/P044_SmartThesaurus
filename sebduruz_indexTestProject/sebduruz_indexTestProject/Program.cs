785a6321a5b1a615d2d4a36f333f6cd544fda3ad
Seb-Dz <sebastien.duruz@eduvaud.ch>
2021-02-19 12:30:37 +0100

    Add a basic (and not finish) way to index file

---
+++ sebduruz_indexTestProject/sebduruz_indexTestProject/Program.cs
@@ -0,0 +1,69 @@
+﻿/// ETML
+/// Autor : Sébastien Duruz
+/// Date : 19.02.2021
+/// Description : A basic test project, try to use indexation functions.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sebduruz_indexTestProject
+{
+    /// <summary>
+    /// Class Program
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Main Method
+        /// </summary>
+        static void Main(string[] args)
+        {
+            foreach (string path in args)
+            {
+                if (File.Exists(path))
+                {
+                    // This path is a file
+                    ProcessFile(path);
+                }
+                else if (Directory.Exists(path))
+                {
+                    // This path is a directory
+                    ProcessDirectory(path);
+                }
+                else
+                {
+                    Console.WriteLine("{0} is not a valid file or directory.", path);
+                }
+            }
+
+            Console.ReadLine();
+        }
+
+        // Process all files in the directory passed in, recurse on any directories
+        // that are found, and process the files they contain.
+        public static void ProcessDirectory(string targetDirectory)
+        {
+            // Process the list of files found in the directory.
+            string[] fileEntries = Directory.GetFiles(targetDirectory);
+            foreach (string fileName in fileEntries)
+                ProcessFile(fileName);
+
+            // Recurse into subdirectories of this directory.
+            string[] subdirectoryEntries = Directory.GetDirectories(targetDirectory);
+            foreach (string subdirectory in subdirectoryEntries)
+                ProcessDirectory(subdirectory);
+        }
+
+        // Insert logic for processing found files here.
+        public static void ProcessFile(string path)
+        {
+            string[] parts = path.Split('\\');
+            path = parts[parts.Count() - 1];
+            Console.WriteLine("Processed file '{0}'.", path);
+        }
+    }
+}
