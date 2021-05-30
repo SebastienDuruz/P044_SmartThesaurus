d8f7e94b8e3e33ae12412f0acb70cf845c8aaaac
Seb-Dz <sebastien.duruz@eduvaud.ch>
2021-05-30 19:49:42 +0200

    Add Unitary Test project to solution

--- P044_SmartThesaurus/P044_SmartThesaurus_UnitTests/FileIndexerTests.cs
+++ P044_SmartThesaurus/P044_SmartThesaurus_UnitTests/FileIndexerTests.cs
@@ -1,27 +1,28 @@
-/// ETML
-/// Autor : Sébastien Duruz
-/// Date : 30.05.2021
-/// Description : Unit test class for FileIndexer file.
-
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using P044_SmartThesaurus.AppBusiness;
 
-namespace P044_SmartThesaurus.P044_SmartThesaurus_UnitTests
+namespace P044_SmartThesaurus_UnitTests
 {
-    /// <summary>
-    /// Class FileIndexerTests
-    /// </summary>
     [TestClass]
     public class FileIndexerTests
     {
         /// <summary>
-        /// Test method of GetInstance()
+        /// Check if GetInstance return a unique instance (check singleton principle)
         /// </summary>
         [TestMethod]
         public void GetInstanceTest()
         {
-            // Act
             // Arrange
+            FileIndexer firstIndexer;
+            FileIndexer secondIndexer;
+            string sourcePath = "./";
+
+            // Act
+            firstIndexer = FileIndexer.GetInstance(sourcePath);
+            secondIndexer = FileIndexer.GetInstance(sourcePath);
+
             // Assert
+            Assert.AreSame(firstIndexer, secondIndexer);
         }
     }
 }
